827430c118d1d71f2e15f04ed18b7813b358fd61
DorinDaniil <dddaniil007@gmail.com>
2024-11-07 12:06:06 +0300

    add 1) invertible gaussian; 2) logistic normal with laplace form of dirichlet approximation

--- src/relaxit/distributions/__init__.py
+++ src/relaxit/distributions/__init__.py
@@ -1,3 +1,9 @@
 from .GaussianRelaxedBernoulli import GaussianRelaxedBernoulli
+from .InvertibleGaussian import InvertibleGaussian
+from .LogisticNormalSoftmax import LogisticNormalSoftmax
 
-__all__ = ["GaussianRelaxedBernoulli"]
+__all__ = [
+    "GaussianRelaxedBernoulli",
+    "InvertibleGaussian",
+    "LogisticNormalSoftmax",
+]
