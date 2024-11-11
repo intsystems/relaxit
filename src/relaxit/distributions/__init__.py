4f671c223de5de33ba98ba0d136eebed04bba9da
DorinDaniil <dddaniil007@gmail.com>
2024-11-11 23:57:34 +0300

    add Correlated Relaxed Bernoulli

--- src/relaxit/distributions/__init__.py
+++ src/relaxit/distributions/__init__.py
@@ -1,5 +1,6 @@
 from .GaussianRelaxedBernoulli import GaussianRelaxedBernoulli
+from .CorrelatedRelaxedBernoulli import CorrelatedRelaxedBernoulli
 from .StraightThroughBernoulli import StraightThroughBernoulli
 from .HardConcrete import HardConcrete
 
-__all__ = ["GaussianRelaxedBernoulli", "StraightThroughBernoulli", "HardConcrete"]
+__all__ = ["GaussianRelaxedBernoulli", "CorrelatedRelaxedBernoulli", "StraightThroughBernoulli", "HardConcrete"]
