7397efce7316ee9a869bbf8bf6a225b9b00c75e9
vepricov <veprikov.as@phystech.edu>
2024-11-26 01:27:51 +0300

    fix topk

--- tests/distributions/test_GumbelSoftmaxTopK.py
+++ tests/distributions/test_GumbelSoftmaxTopK.py
@@ -6,16 +6,32 @@ from relaxit.distributions.GumbelSoftmaxTopK import GumbelSoftmaxTopK
 # Testing reparameterized sampling and log prob from the GumbelSoftmaxTopK distribution
 
 def test_sample_shape():
-    a = torch.tensor([[1, 2, 3]])
-    distribution = GumbelSoftmaxTopK(a, K=2)
+    a = torch.tensor([1., 2., 3., 4., 5.])
+    K = torch.tensor(2)
+    tau = torch.tensor(0.1)
+    distribution = GumbelSoftmaxTopK(a, K=K, tau=tau)
     sample = distribution.rsample()
-    assert sample.shape == torch.Size([2])
-    print("$")
+    assert sample.shape == a.shape
+
+def test_sample_grad():
+    a = torch.tensor([1., 2., 3., 4., 5.], requires_grad=True)
+    K = torch.tensor(2)
+    tau = torch.tensor(0.1)
+    distribution = GumbelSoftmaxTopK(a, K=K, tau=tau)
+    sample = distribution.rsample()
+    assert sample.requires_grad == True
     
 def test_log_prob():
     a = torch.tensor([1., 2., 3.])
-    distribution = GumbelSoftmaxTopK(a, K=1)
-    value = 1
-    log_prob_my = distribution.log_prob(value, shape=torch.Size([])) 
-    log_prob_true = torch.log(a[value] / 6.)
-    assert log_prob_my - log_prob_true < 1e-6
+    K = torch.tensor(3)
+    tau = torch.tensor(0.1)
+    distribution = GumbelSoftmaxTopK(a, K=K, tau=tau)
+    sample = distribution.rsample()
+    value = torch.tensor([1., 1., 1.])
+    log_prob = distribution.log_prob(value) 
+    assert log_prob - torch.tensor(0) < 1e-6
+
+if __name__ == "__main__":
+    test_sample_shape()
+    test_sample_grad()
+    test_log_prob()
